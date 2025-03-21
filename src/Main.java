50319f04556ba5000ab6d86a1d90c490d5828567
Poke <willmc033@gmail.com>
2025-03-20 19:38:57 -0600

    Creacion de Menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,46 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hola");
+        Scanner scanner = new Scanner(System.in);
+        //Lista de Productos
+        List<String> productos = new ArrayList<>();
+        productos.add("Producto 1");
+        System.out.println(productos.get(0));
+
+        //Declaracion de Variables
+        byte menu;
+        boolean exit= false;
+
+
+
+        //Menu Principal
+        do {
+            System.out.println("******* Opciones *******");
+            System.out.println("1. Agregar Producto.");
+            System.out.println("2. Mostrar Productos.");
+            System.out.println("3. Actualizar cantidad de un Producto.");
+            System.out.println("4. Eliminar Producto");
+            System.out.println("5. Salir");
+            menu = scanner.nextByte();
+
+            switch (menu) {
+                case 1:
+                    System.out.println("Caso 1");
+                    break;
+                case 2:
+                    System.out.println("Caso 2");
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    exit = true;
+                    break;
+            }
+        }while (exit == false);
     }
 }
